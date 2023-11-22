d0a60d24ea3211173a7c930f3e6ae4f7e23bdee7
NursB <scoobybest5@gmail.com>
2023-11-22 23:19:00 +0600

    Added inheritance1

---
+++ inheritance1.py
@@ -0,0 +1,43 @@
+class Figure():       
+
+    def square(self):
+        pass
+
+
+class Kvadrat(Figure):
+    def __init__(self, side) -> None:
+        super().__init__()
+        self.side = side
+
+    def square(self,side):
+        return side**2
+
+
+class Rectangle(Figure):
+    def __init__(self, shirina, visota) -> None:
+        super().__init__()
+        self.shirina = shirina
+        self.visota = visota
+
+    def square(self,shirina,visota):
+        return visota*shirina
+
+
+class triangle(Figure):
+    def __init__(self, visota, osnova) -> None:
+        super().__init__()
+        self.visota = visota
+        self.osnova = osnova
+
+    def square(self,visota, osnova):
+        return 2*visota*0.5*osnova
+
+
+kv = Kvadrat(2)
+print(f'площадь квадрата = {kv.square(kv.side)}')
+
+rc = Rectangle(2,4)
+print(f'площадь прямоугольника = {rc.square(rc.shirina, rc.visota)}')
+
+tr = triangle(2,4)
+print(f'площадь треугольника = {tr.square(tr.visota, tr.osnova)}')
